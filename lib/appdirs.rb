21a3e2da3690d588e884364156a98dbc9c50741f
phonghpham <9318603+phonghpham@users.noreply.github.com>
2024-02-23 16:45:53 -0600

    initial commit

---
+++ lib/appdirs.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "appdirs/version"
+
+module Appdirs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/appdirs/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Appdirs
+  VERSION = "0.1.0"
+end
